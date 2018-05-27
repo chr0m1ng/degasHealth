9186c874097ff6a465b993c21149a166e7f5b969
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-05-27 18:54:30 -0300

    Add database sql

---
+++ DBCreator.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE hospital;
+
+USE hospital;
+
+CREATE TABLE Medico (
+  id   INTEGER     NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  crm  INTEGER     NOT NULL,
+  sexo CHAR        NOT NULL,
+  nacionalidade VARCHAR(30) NOT NULL,
+  dataNasc DATE NOT NULL,
+  dataAdmiss DATE NOT NULL,
+  dataFormatura DATE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM Medico;
+
+drop TABLE Medico;
